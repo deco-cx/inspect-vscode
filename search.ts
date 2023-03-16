11e6c4862ff679608727b879fc1e29199bf266d2
gimenes <tlgimenes@gmail.com>
2023-03-16 10:29:46 -0300

    fix: open line:1 col:1 when text not found

--- search.ts
+++ search.ts
@@ -7,8 +7,8 @@ export async function search(content: string, manifestKey: string) {
   const srcLines = srcFile.split("\n");
   const fuse = new Fuse(srcLines);
   const [bestResult] = fuse.search(escaped);
-  const lineNumber = bestResult.refIndex + 1;
-  const column = bestResult.item.search(/\S/) + 1;
+  const lineNumber = (bestResult?.refIndex ?? 0) + 1;
+  const column = (bestResult?.item.search(/\S/) ?? 0) + 1;
   const fullpath = join(Deno.cwd(), manifestKey);
 
   return { fullpath, lineNumber, column };
